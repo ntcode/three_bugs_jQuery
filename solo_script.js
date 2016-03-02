d717b808f21ea70b4ee573f94eab8601a64b16ff
ntcode <ntskupa@gmail.com>
2016-03-02 08:33:08 -0600

    change: calculate STI & function Person

--- solo_script.js
+++ solo_script.js
@@ -1,49 +1,64 @@
 // ! ! !
 // Three Bugs
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
-
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+//var arrayAtticus = ["Atticus", "2405", "47000", 3];
+//var arrayJem = ["Jem", "62347", "63500", 4];
+//var arrayBoo = ["Boo", "11435", "54000", 3];
+//var arrayScout = ["Scout", "6243", "74750", 5];
+//arrays have been attached to function Person to input property names for each cell
+var Atticus = new Person("Atticus", "2405", "47000", 3);
+var Jem = new Person("Jem", "62347", "63500", 4);
+var Boo = new Person("Boo", "11435", "54000", 3);
+var Scout = new Person("Scout", "6243", "74750", 5);
+//var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+var personArray = [Atticus, Jem, Boo, Scout];
+var adjustedSalary = [];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
-
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array[i]);
+
+console.log(personArray);//logs out 4 new Person arrays within personArray
+
+for (var i = 0; i < personArray.length; i++) {
+  adjustedSalary[i] = calculateSTI(personArray[i]);
+}//loops through personArray containing 4 arrays
+//attaching property names to each cell in person arrays above
+
+function Person(name, id, salary, rank) {
+  this.name = name;
+  this.id = id;
+  this.salary = salary;
+  this.rank = rank;
 }
 
-function calculateSTI(array){
-  var newArray = [];
-
-  newArray[0] = array[0];
-
-  var employeeNumber = array[1];
-  var baseSalary = parseInt(array[2]);
-  var reviewScore = array[3];
-
-  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
-  if(bonus > 0.13){
+function calculateSTI(array) {
+  //newArray[0] = array[0];
+  //var employeeNumber = array[1];
+  //var baseSalary = parseInt(array[2]);
+  //var reviewScore = array[3];
+  var name = array.name;
+  var employeeNumber = array.id;//places employeeNumber of each array into object
+  var reviewScore = array.rank;//places ranking of each array into object
+  var baseSalary = parseInt(array.salary);//turns string to intiger and places salary of each array into object
+  var bonusPercent = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if (bonus > 0.13) {
     bonus = 0.13;
   }
 
+  var roundedSalary = Math.round(baseSalary);
+  var newSalary = parseInt(roundedSalary * (1.0 + bonusPercent));
+  var bonus = parseInt(roundedSalary * bonusPercent);
 
-  newArray[1] = bonus;
-  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
-  newArray[3] = Math.round(baseSalary * bonus);
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-  return newArray;
+  return {name, baseSalary, bonus, newSalary};
 }
 
-function getBaseSTI(reviewScore){
+function getBaseSTI(reviewScore) {
   var basePercent;
-  switch(reviewScore){
+  switch (reviewScore) {
     case 1:
       basePercent = 0;
       break;
@@ -67,54 +82,33 @@ function getBaseSTI(reviewScore){
   return basePercent;
 }
 
-function getYearAdjustment(employeeNumber){
+function getYearAdjustment(employeeNumber) {
   var yearAdjustment = 0;
-  if(employeeNumber.length == 4){
+  if (employeeNumber.length == 4) {
     yearAdjustment = 0.05;
   }
+
   return yearAdjustment;
 }
 
-function getIncomeAdjustment(salary){
+function getIncomeAdjustment(baseSalary) {
   var incomeAdjustment = 0;
-  salary = parseInt(salary);
-  if(salary > 65000){
+  if (baseSalary > 65000) {
     incomeAdjustment = 0.01;
   }
+
   return incomeAdjustment;
 }
-var peopleArray = [];
 
-var arrayAtticus = new People("Atticus", "47000", "4230", "51230");
-var arrayJem = new People("Jem", "63500", "3810", "67310");
-var arrayBoo = new People("Boo", "54000", "2160", "56160");
-var arrayScout = new People("Scout", "74750", "9718", "84467");
+$(document).ready(function () {
+  for (var i = 0; i < adjustedSalary.length; i++) {
+    $('.container').append('<div class="person"></div>');
 
-$(document).ready(function(){
-  for(var i = 0; i < peopleArray.length; i++){
-    appendDom(peopleArray[i]);
+    var $el = $('.container').children().last();
+
+    $el.append('<p>' + adjustedSalary[i].name + '</p>');
+    $el.append('<p>' + adjustedSalary[i].baseSalary + '</p>');
+    $el.append('<p>' + adjustedSalary[i].bonus + '</p>');
+    $el.append('<p>' + adjustedSalary[i].newSalary + '</p>');
   }
 });
-
-function People (name, salary, bonus, totalCompensation){
-  this.name = name;
-  this.salary = salary;
-  this.bonus = bonus;
-  this.totalCompensation = totalCompensation; 
-  peopleArray.push(this);
-}
-
-function appendDom(object){
-  $('.container').append('<div class="person"></div>');
-
-  var $el = $('.container').children().last();
-
-  $el.append('<p>' + object.name + '</p>');
-  $el.append('<p>' + object.salary + '</p>');
-  $el.append('<p>' + object.bonus + '</p>');
-  $el.append('<p>' + object.totalCompensation + '</p>');
-}
-
-
-
-
